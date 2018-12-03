471f274b8f1b5a1b74bfe92851e8191f411ed83f
mattrivera <rivera182matty@gmail.com>
2018-12-02 23:39:15 -0500

    media queries in progress

--- src/ToDo.js
+++ src/ToDo.js
@@ -6,7 +6,7 @@ const todo = (props) => {
             <p className="catName">{props.cat}</p>
             <h3 className="toDoTitle">{props.title}</h3>
             <p classsName="toDoTask">{props.task}</p>
-            <button id={props.keyId} onClick={props.click} className="completedTask"><i class="fas fa-check-square"></i></button>
+            <button id={props.keyId} onClick={props.click} className="completedTask"><i class="fas fa-times-circle"></i></button>
         </div>
     )
 }
